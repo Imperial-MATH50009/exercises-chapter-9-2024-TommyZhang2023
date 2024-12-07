3a22cc6eb930c6b7bca4a21fba70281cbcaf147a
tommy.z <tommy.zhang23@imperial.ac.uk>
2024-12-07 19:02:05 +0000

    ex1.2

--- expressions/__init__.py
+++ expressions/__init__.py
@@ -1,2 +1,2 @@
 """Make packages directly accessible."""
-from .expression import Symbol, Number, Add, Sub, Mul, Div, Pow # noqa F401
+from .expressions import Symbol, Number, Add, Sub, Mul, Div, Pow # noqa F401
